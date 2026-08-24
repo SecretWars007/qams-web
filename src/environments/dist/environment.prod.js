b103d3d061bcaf37bd42d141b06ae3cc68c128f3
SecretWars007 <enriquezariasedwingustavo@gmail.com>
2026-08-23 21:09:35 -0400

    fix(render): remover region en blueprint de static site y actualizar api url de produccion

---
+++ src/environments/dist/environment.prod.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+exports.environment = void 0;
+exports.environment = {
+    production: true,
+    apiUrl: 'https://qams-api-6asw.onrender.com/api'
+};
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://qams-api.onrender.com/api',
+  apiUrl: 'https://qams-api-6asw.onrender.com/api',
 };
